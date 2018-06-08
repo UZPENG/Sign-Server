e2340a9a22b245739d4f8e9deae15847da81afe5
uzpeng <liu@uzpeng.top>
2018-06-08 16:58:10 +0800

    修改一些bean的信息

---
+++ src/main/java/com/uzpeng/sign/bo/SignRecordTimeListBO.java
@@ -0,0 +1,18 @@
+package com.uzpeng.sign.bo;
+
+import java.util.List;
+
+/**
+ * @author uzpeng on 2018/4/17.
+ */
+public class SignRecordTimeListBO {
+    private List<SignRecordTimeBO> list;
+
+    public List<SignRecordTimeBO> getList() {
+        return list;
+    }
+
+    public void setList(List<SignRecordTimeBO> list) {
+        this.list = list;
+    }
+}
